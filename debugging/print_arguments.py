5b90964ee15ea814fd329691544eafd7e5b00833
root <root@ip-10-42-248-67.ec2.internal>
2026-01-12 08:59:06 +0000

    bon

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-def main() -> int:
-    print(f"Programme: {sys.argv[0]}")
-    print("Arguments:")
-    for arg in sys.argv[1:]:
-        print(f"  - {arg}")
-    return 0
-
-if __name__ == "__main__":
-    raise SystemExit(main())
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
